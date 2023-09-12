07861658a9003577a0963aaec6674329ae909bc5
Vincent Goudard <vincentgoudard@gmail.com>
2023-09-12 15:59:00 +0200

    test unit conversion

---
+++ javascript/mvc.unit-converter.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const Max = require('max-api');
+const convert = require('convert');
+
+// This will be printed directly to the Max console
+Max.post(`Loaded the ${path.basename(__filename)} script`);
+
+// Use the 'addHandler' function to register a function for a particular message
+Max.addHandler("bang", () => {
+	Max.post("Who you think you bangin'?");
+});
+
+// Use the 'outlet' function to send messages out of node.script's outlet
+Max.addHandler("echo", (msg) => {
+	var result = convert.convert(msg, 'radian').to('degrees');
+	Max.outlet(result);
+});
