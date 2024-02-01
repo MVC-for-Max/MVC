bfddd395e4b3a6e757834bb5ed47b62a2b639afa
Vincent Goudard <vincentgoudard@gmail.com>
2024-02-01 12:34:43 +0100

    interpolate only if necessary

--- javascript/mvc.dict.interpolate.js
+++ javascript/mvc.dict.interpolate.js
@@ -11,28 +11,37 @@ var rightDictObj = null;
 
 var resultDict = new Dict();
 
+var shouldRecompute = 1;
+var interpValue = -1.;
 
 function msg_float(v){
 	
-	var interpValue;
+
 	if (typeof v === "number"){
-		interpValue = Math.min(Math.max(v, 0.), 1.);
-		//post("received float " + interpValue + "\n");
+		var tmp = Math.min(Math.max(v, 0.), 1.);
+		if (tmp!=interpValue) {
+			interpValue = tmp;
+			shouldRecompute = 1;
+			//post("received float " + interpValue + "\n");
+		}
 	}
 	else return;
+	//post("shouldRecompute" , shouldRecompute, "\n");
 	
-	var interpolatingDictObj;
+	if (shouldRecompute){
+		var interpolatingDictObj;
 	
-	if (interpValue<0.5) {
-		interpolatingDictObj = JSON.parse(leftDict.stringify()); // could be interpolatingDictObj = leftDictObj ?
-		mergeDeep(interpolatingDictObj, rightDictObj,  interpValue);
+		if (interpValue<0.5) {
+			interpolatingDictObj = JSON.parse(leftDict.stringify()); // could be interpolatingDictObj = leftDictObj ?
+			mergeDeep(interpolatingDictObj, rightDictObj,  interpValue);
+		}
+		else {
+			interpolatingDictObj = JSON.parse(rightDict.stringify()); // could be interpolatingDictObj = rightDictObj ?
+			mergeDeep(interpolatingDictObj, leftDictObj, 1.-interpValue);
+		}
+		resultDict.parse(JSON.stringify(interpolatingDictObj));
+		shouldRecompute = 0;
 	}
-	else {
-		interpolatingDictObj = JSON.parse(rightDict.stringify()); // could be interpolatingDictObj = rightDictObj ?
-		mergeDeep(interpolatingDictObj, leftDictObj, 1.-interpValue);
-	}
-			
-	resultDict.parse(JSON.stringify(interpolatingDictObj));
 	outlet(0, "dictionary", resultDict.name);
 }
 
@@ -41,12 +50,14 @@ function dictionary(dictName) {
 		rightDict.name = dictName;
 		rightDictObj = JSON.parse(rightDict.stringify());
 		//outlet(1, "dictionary", rightDict.name);
+		shouldRecompute = 1;
 		return;
 	}
 	if (inlet == 1) {
 		leftDict.name = dictName;
 		leftDictObj = JSON.parse(leftDict.stringify());
 		//outlet(1, "dictionary", leftDict.name);
+		shouldRecompute = 1;
 		return;
 	}
 }
