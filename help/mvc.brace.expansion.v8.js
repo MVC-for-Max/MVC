a799b49880b2b657bd2abae3c2b90b09bd5fb6be
Vincent Goudard <vincentgoudard@gmail.com>
2026-01-07 11:42:58 +0100

    add address array expansion

--- help/mvc.brace.expansion.v8.js
+++ help/mvc.brace.expansion.v8.js
@@ -1,12 +1,115 @@
 inlets = 1;
 outlets = 1;
 
+let childdict = new Dict();
+childdict.quiet = 1;
+
+let parentdict = new Dict();
+parentdict.quiet = 1;
+
+
 function braceExpand(str) {
   const ast = parse(str);
+  //post('the chain:', ast, '\n');
   outlet(0, expandSeq([""], ast)); 
   return;// expandSeq([""], ast);
 }
 
+function braceExpandArray(strArray){
+    
+    let resultArray = [];
+    strArray.forEach(function (item, index) {
+        const ast = parse(item);
+        //post('toto', ast, '\n');
+        var res = expandSeq([""], ast);
+        //post('res', res, '\n');
+        resultArray.push(res);
+    });
+    
+    return resultArray;
+}
+
+
+/* ---------------- EXPAND AND DISTRIBUTE OVER PARENT NAMESPACE ---------------- */
+
+function braceExpandOnParents(attrDictName)
+{
+	childdict.name = attrDictName;
+		
+    var uid = childdict.get('uid');;
+    //post('uid:', uid, '\n');
+    var parentUid = childdict.get('parent');
+    //post('parentuid:', uid, '\n');
+    
+    parentdict.name = parentUid + '.attr';
+    
+    // fetch address to expand make sure it's an array
+	var addressToExpand = childdict.get('address') ?? [];
+	addressToExpand = Array.isArray(addressToExpand) ? addressToExpand : [addressToExpand];
+    //post('addressToExpand:', addressToExpand, '\n');
+
+	// fetch parent addresses and make sure it's an array
+	var parentAddresses = parentdict.get('addresslist') ?? [];
+	parentAddresses = Array.isArray(parentAddresses) ? parentAddresses : [parentAddresses];
+	//post('parentAddresses', parentAddresses);
+
+	var expandedAddresses;
+	expandedAddresses = braceExpandArray(addressToExpand);
+    childdict.replace('expandedAddresses', expandedAddresses);
+	//post('expandedAddresses', expandedAddresses);
+    
+    // DISTRIBUTE EXPANDED ADDRESSES OVER PARENT NODE'S ADDRESSES
+    var adddressIndex = 0; //the address index in the final addresslist
+	var addresslist = []; // the final address list
+	var parentmap = [];		// an array mapping each address to the index of the parent addresses
+	var childrenmap = []; // a nested array mapping parent index to an array of corresponding child addresses indices
+
+	if (parentdict.get('uid') == null){ // If no parent, consider the relative address as absolute
+		addresslist = expandedAddresses.flat();
+		//Max.post('addresslist flattened', addresslist);
+		var tmpArray = [];
+		for (var i = 0; i < addresslist.length; i++) {
+   		parentmap.push(1);
+   		tmpArray.push(i+1);
+		}
+		childrenmap.push(tmpArray);
+	}	
+	else if (parentdict.get('uid') != childdict.get('uid')){ //concat on parent address
+		for (let i = 0; i < parentAddresses.length; i++) {
+			var childIndexArray = [];
+			var addressesArrayForThisParentAddress = expandedAddresses[i%expandedAddresses.length];
+			//Max.post('addressesArrayForThisParentAddress', addressesArrayForThisParentAddress);
+			for (let j = 0; j < addressesArrayForThisParentAddress.length; j++) {
+				var childAdd = addressesArrayForThisParentAddress[j];
+				if (childAdd!= 'none'){
+					var concatAddress = parentAddresses[i] + '/' + childAdd;
+					//Max.post('concatAddress', concatAddress);
+					addresslist.push(concatAddress);					
+					childrenmap.push(i+1);
+					adddressIndex++;
+					childIndexArray.push(adddressIndex);
+				}
+				else{
+					//Max.post("youhou",childAdd )
+				}
+			}
+			parentmap.push(childIndexArray);
+		}
+		//addresslist = ["addresslist"].concat(addresslist);
+	}
+	else { //this is the device
+		//addresslist = ["addresslist"].concat(expandedAddresses[0]);
+		addresslist = expandedAddresses[0];
+		parentmap = [1];
+		childrenmap = [1];
+	}
+
+	childdict.replace('addresslist', addresslist);
+    childdict.replace('parentmap', parentmap);
+    childdict.replace('childrenmap', childrenmap);
+
+	outlet(0, 'bang');
+}
 
 /* ---------------- PARSEUR ---------------- */
 
