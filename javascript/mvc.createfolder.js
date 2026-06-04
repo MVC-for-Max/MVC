b68d4cc9d10fc051736846b2fa0d3ce04de52f6e
Vincent Goudard <vincentgoudard@gmail.com>
2026-06-04 11:50:17 +0200

    AI optimize

--- javascript/mvc.createfolder.js
+++ javascript/mvc.createfolder.js
@@ -3,30 +3,18 @@ const path = require('path');
 const Max = require('max-api');
 
 Max.addHandler("createfolder", (thepath, foldername) => {
-		
-	var newpath = thepath + foldername;
-	
-	// test if folder exists
-	fs.access(newpath, function(error) {
-		if (error) {
-			Max.outlet(foldername, ' folder does not exist.');
-			//console.log("Data folder does not exist.")
-			fs.mkdir(newpath, { recursive: true }, function(err) {
-  				if (err) {
-    				//console.log(err)
-					Max.outlet('ERROR','•', 'Failed creating', foldername, 'folder:', newpath);
-  				} else {
-    			//console.log("New directory successfully created:", newpath)
-					Max.outlet('Created',foldername, 'folder:', newpath);
-  					Max.outlet('SUCCESS',foldername);
-				}
-			})
-		} else {
-			Max.outlet(foldername, 'folder found.');
-			Max.outlet('SUCCESS', foldername);
-		}
-	})
 
+    const newpath = path.join(thepath, foldername);
+
+    fs.mkdir(newpath, { recursive: true }, (err) => {
+
+        if (err) {
+            Max.outlet('ERROR','•','Failed creating', foldername,':', err.message);
+            return;
+        }
+
+        Max.outlet('SUCCESS', foldername);
+    });
 });
 
 Max.outlet('ready');
